7aa37b22f642af8c08e893aa09de4e6033429cb3
n8te.98 <nathan.vaubien@gmail.com>
2022-11-02 17:10:00 +0100

    Update

---
+++ relist.py
@@ -0,0 +1,756 @@
+import pyautogui as py
+import time
+import webbrowser
+
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
+
+# Ouvrir la webapp FUT
+webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
+
+time.sleep(25)
+
+py.leftClick(54, 475) #Position onglet transfert
+
+time.sleep(10)
+
+
+py.leftClick(777, 749) #Position onglet list transfert
+
+time.sleep(15)
+
+
+py.leftClick(1183, 337) #Position clear sold
+
+time.sleep(17)
+
+py.leftClick(1185, 542) #Position relist all
+
+time.sleep(10)
+
+py.leftClick(911, 664) #Confirmer relisting
+
+py.leftClick(1903, 47) #Fermer le browser
+
+time.sleep(3600)
+
+################### RELISTING EN COURS ###################
