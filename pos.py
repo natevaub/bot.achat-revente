4d316797a5ba636b4bf9f0c22015ba675ead841b
n8te.98 <nathan.vaubien@gmail.com>
2022-11-03 15:11:19 +0100

    Update

---
+++ pos.py
@@ -0,0 +1,7 @@
+
+import pyautogui as py
+import time
+import webbrowser
+
+time.sleep(8)
+print(py.position()) 
--- relist.py
+++ relist.py
@@ -6,27 +6,32 @@ while (True):
     # Ouvrir la webapp FUT
     webbrowser.open("https://www.ea.com/fifa/ultimate-team/web-app/")
 
-    time.sleep(25)
+    time.sleep(10)
 
-    py.leftClick(54, 475) #Position onglet transfert
+    py.leftClick(1278, 637) #Position onglet transfert
+
+    time.sleep(25)
+    
+
+    py.leftClick(48, 439) #Position onglet transfert
 
     time.sleep(10)
 
 
-    py.leftClick(777, 749) #Position onglet list transfert
+    py.leftClick(790, 734) #Position onglet list transfert
 
     time.sleep(15)
 
 
-    py.leftClick(1183, 337) #Position clear sold
+    py.leftClick(1191, 304) #Position clear sold
 
     time.sleep(17)
 
-    py.leftClick(1185, 542) #Position relist all
+    py.leftClick(1196, 514) #Position relist all
 
     time.sleep(10)
 
-    py.leftClick(911, 664) #Confirmer relisting
+    py.leftClick(931, 645) #Confirmer relisting
 
     time.sleep(10)
 
